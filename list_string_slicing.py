a71d2ec6ec5dc2eff9f4f926d9ff986271cd7aed
Rohini Kumar Barla <rohinibarla@hey.com>
2021-03-12 20:51:47 +0530

    sample code 12 march

---
+++ list_string_slicing.py
@@ -0,0 +1,11 @@
+names = ['varaprasad', 'manikanta', 'siva']
+names[1] = "kalyan"
+names.append("Sai")
+peru = names.pop(0)
+print(peru)
+print(len(names))
+print(len(peru))
+
+file_name = "Python_wewecsk2ddw.mp4"
+print(file_name[-2000:-4])
+
